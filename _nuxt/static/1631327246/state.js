92def0fd46ef9362afae17644c6b9baebd4be23f
Kathy Kuo <dudoo.kathy@gmail.com>
2021-09-11 10:28:48 +0800

    1e317ee

---
+++ _nuxt/static/1631327246/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FFlowerShop\u002F_nuxt\u002Fstatic\u002F1631327246",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002FFlowerShop\u002F",assetsPath:"\u002FFlowerShop\u002F_nuxt\u002F",cdnURL:a}}}}(null));
